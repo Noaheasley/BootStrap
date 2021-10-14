b17a57d2d5527d58a166ba5da53530ba88673f70
Noah <Noaheasley408@gmail.com>
2021-10-14 16:58:00 -0500

    added the CUBE

--- GraphicsProject/BuddhaWorld.cpp
+++ GraphicsProject/BuddhaWorld.cpp
@@ -11,23 +11,23 @@ void BuddhaWorld::onStart()
 
 	//Light
 	m_light = new Light(
-		{ 1.0f, 1.0f, 1.0f },
-		{ 1.0f, 0.5f, 0.5f, 1.0f },
+		{ 0.6f, 0.4f, 1.0f },
+		{ 0.5f, 0.5f, 0.5f, 1.0f },
 		{ 1.0f, 1.0f, 1.0f, 1.0f },
-		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		{ 0.5f, 0.5f, 0.5f, 0.5f }
 	);
 	m_light->setDirection(glm::vec3{ -60, -60, -60 });
 	add(m_light);
 
 	//Cube
 	m_cube = new Cube();
-	m_cube->setColor(glm::vec4(0.6f, 0.2f, 1.0f, 1.0f));
+	m_cube->setColor(glm::vec4(0.3f, 0.6f, 1.0f, 1.0f));
 	add(m_cube);
 
 	//Dragon
 	m_buddha = new OBJMesh();
 	m_buddha->load("Buddha.obj");
-	m_buddha->getTransform()->setPosition({ 5.0f, 0.0f, 0.0f });
+	m_buddha->getTransform()->setPosition({ 0.0f, -1.0f, 0.0f });
 	m_buddha->getTransform()->setScale(glm::vec3(0.1f));
 	add(m_buddha);
 }
--- GraphicsProject/Cube.cpp
+++ GraphicsProject/Cube.cpp
@@ -3,22 +3,75 @@
 Cube::Vertex* Cube::generateVertices(unsigned int& vertexCount, unsigned int& triCount)
 {
 	Vertex* vertices = nullptr;
-	triCount = 12;
 	vertexCount = 36;
+	triCount = 12;
 
+	//Define vertices for a quad
 	vertices = new Vertex[vertexCount];
 
-	setVertexPosition(vertices, vertexCount, 0, { -1.0f,  1.0f, -1.0f }); //A
-	setVertexPosition(vertices, vertexCount, 1, { 1.0f,  1.0f,  1.0f }); //B
-	setVertexPosition(vertices, vertexCount, 2, { 1.0f, -1.0f, -1.0f }); //C
-	setVertexPosition(vertices, vertexCount, 3, { -1.0f, -1.0f,  1.0f }); //D
-	setVertexPosition(vertices, vertexCount, 4, { 1.0f,  1.0f, -1.0f }); //E
-	setVertexPosition(vertices, vertexCount, 5, { -1.0f,  1.0f,  1.0f }); //F
-	setVertexPosition(vertices, vertexCount, 6, { -1.0f, -1.0f, -1.0f }); //G
-	setVertexPosition(vertices, vertexCount, 7, { 1.0f, -1.0f,  1.0f }); //H
+	////Top Face
+	//triangle 0
+	vertices[0].position = { 1.0f, 1.0f,  1.0f, 1.0f };
+	vertices[1].position = { -1.0f, 1.0f,  1.0f, 1.0f };
+	vertices[2].position = { -1.0f, 1.0f,  -1.0f, 1.0f };
+	//triangle 1
+	vertices[3].position = { 1.0f, 1.0f,  1.0f, 1.0f };
+	vertices[4].position = { 1.0f, 1.0f, -1.0f, 1.0f };
+	vertices[5].position = { -1.0f, 1.0f, -1.0f, 1.0f };
+
+	////Bottom Face
+	//triangle 0
+	vertices[6].position = { -0.5f, 0.0f, 0.5f, 1.0f };
+	vertices[7].position = { 0.5f, 0.0f, 0.5f, 1.0f };
+	vertices[8].position = { -0.5f, 0.0f, -0.5f, 1.0f };
+	//triangle 1
+	vertices[9].position = { 0.5f, 0.0f, 0.5f, 1.0f };
+	vertices[10].position = { -0.5f, 0.0f, -0.5f, 1.0f };
+	vertices[11].position = { 0.5f, 0.0f, -0.5f, 1.0f };
+
+	////Right Face
+	//triangle 0
+	vertices[12].position = { -1.0f, 1.0f, 1.0f, 1.0f };
+	vertices[13].position = { -1.0f, -1.0f,  -1.0f, 1.0f };
+	vertices[14].position = { -1.0f, 1.0f,  -1.0f, 1.0f };
+	//triangle 1
+	vertices[15].position = { -1.0f, -1.0f, 1.0f, 1.0f };
+	vertices[16].position = { -1.0f, -1.0f,  -1.0f, 1.0f };
+	vertices[17].position = { -1.0f, 1.0f,  1.0f, 1.0f };
+
+	////Left Face
+	//triangle 0
+	vertices[18].position = { 1.0f, -1.0f, -1.0f, 1.0f };
+	vertices[19].position = { 1.0f, 1.0f,  1.0f, 1.0f };
+	vertices[20].position = { 1.0f, 1.0f,  -1.0f, 1.0f };
+	//triangle 1
+	vertices[21].position = { 1.0f, -1.0f,  1.0f, 1.0f }; 
+	vertices[22].position = { 1.0f, -1.0f,  -1.0f, 1.0f }; 
+	vertices[23].position = { 1.0f, 1.0f,  1.0f, 1.0f }; 
+
+	////Back Face
+	//triangle 0
+	vertices[24].position = { -1.0f, -1.0f, -1.0f, 1.0f };
+	vertices[25].position = { 1.0f, -1.0f,  -1.0f, 1.0f };
+	vertices[26].position = { 1.0f, 1.0f,  -1.0f, 1.0f };
+	//triangle 1
+	vertices[27].position = { -1.0f, -1.0f,  -1.0f, 1.0f };
+	vertices[28].position = { -1.0f, 1.0f,  -1.0f, 1.0f };
+	vertices[29].position = { 1.0f, 1.0f,  -1.0f, 1.0f };
+
+	////Front Face
+	//triangle 0
+	vertices[30].position = { -1.0f, -1.0f,  1.0f, 1.0f }; 
+	vertices[31].position = { 1.0f, -1.0f,  1.0f, 1.0f }; 
+	vertices[32].position = { 1.0f, 1.0f,  1.0f, 1.0f }; 
+	//triangle 1
+	vertices[33].position = { -1.0f, -1.0f,  1.0f, 1.0f}; 
+	vertices[34].position = { -1.0f, 1.0f,  1.0f, 1.0f }; 
+	vertices[35].position = { 1.0f, 1.0f,  1.0f, 1.0f }; 
+
+
 
 	for (int i = 0; i < vertexCount; i++) {
-		vertices[i].normal = { 0.0f, 1.0f, 0.0f, 0.0f };
 		vertices[i].color = m_color;
 	}
 
